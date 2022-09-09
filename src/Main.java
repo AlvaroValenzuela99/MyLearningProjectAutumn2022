24d1f299df45a215121248ce4222ab22e3e1805e
Alvaro Valenzuela <alvarovgerena@gmail.com>
2022-09-09 18:55:21 +0200

    Added implementation of Exercise 2.1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,18 @@ public class Main {
         sum = x + y;
 
         System.out.println(sum);
-        System.out.println("El resultado de la suma es: " +sum);
+        System.out.println("Sum of " + x + " and " + y + " equals " + sum);
+
+        if (sum == 9 || y == 5){
+            System.out.println("Sum is 9");
+        }
+        else if (sum <= 8){
+            System.out.println("Sum is less than or equal to 8");
+        }
+        else {
+            System.out.println("We're not so good. :(");
+        }
+
+        System.out.println("The new value of the sum is " + sum);
     }
 }
