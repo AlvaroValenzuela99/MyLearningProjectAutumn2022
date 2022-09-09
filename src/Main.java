15c6143ddb7dd6e71cd652d843036b2b69f45ac4
Alvaro Valenzuela <alvarovgerena@gmail.com>
2022-09-09 17:31:40 +0200

    Add the implementation of Exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        // System.out.println("Hello World");
-        System.out.print("Hello World");
+        int x = 4;
+        int y = 5;
 
+        int sum;
+
+        sum = x + y;
+
+        System.out.println(sum);
+        System.out.println("El resultado de la suma es: " +sum);
     }
 }
