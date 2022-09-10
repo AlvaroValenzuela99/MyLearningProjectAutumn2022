674d6e56d6d8307dc3ed5f9d22a9aaace66516be
Alvaro Valenzuela <alvarovgerena@gmail.com>
2022-09-10 11:16:13 +0200

    Add the implementation of exercise 4, array and for loop

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int dayOfWeek = 3;
+        int [] numbers = {1, 3, 5, 7, 9};
 
-        switch (dayOfWeek){
-            case 1:
-                System.out.println("Its Monday!");
-                break;
-            case 2:
-                System.out.println("Its Tuesday!");
-                break;
-            case 3:
-                System.out.println("Its Wednesday!");
-                break;
-            case 4:
-                System.out.println("Its Thursday!");
-                break;
-            case 5:
-                System.out.println("Its Friday!");
-                break;
-            case 6:
-                System.out.println("Its Saturday!");
-                break;
-            case 7:
-                System.out.println("Its Sunday!");
-                break;
-            default:
-                System.out.println("We have only 7 daus in a week");
+        System.out.println(numbers[2]);
+
+        String[] artists = {"Raphael", "da Vinci", "Picasso", "Meonet", "Hopper", "Munch"};
+        boolean[] valid = {false, false, true, false, true, true};
+
+        System.out.println("Length of artists array: " + artists.length);
+
+        for (int i = 0; i < artists.length; i++){
+            if (valid[i]){
+                System.out.println(artists[i]);
+            }
         }
     }
 }
