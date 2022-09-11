3f79bfd12691d778776ccde2280602ffa6f98940
Alvaro Valenzuela <alvarovgerena@gmail.com>
2022-09-11 09:12:34 +0200

    Add an implementation of Exercise 7, adding print and calculateCalories methods to our classes

--- src/vending_machine/Beverage.java
+++ src/vending_machine/Beverage.java
@@ -5,4 +5,16 @@ public class Beverage {
     boolean isHot;
     boolean isSparkling;
 
+    void print(){
+        System.out.println("-------------------");
+        System.out.println(name);
+        if (isHot){
+            System.out.println("CAUTION! The drink is hot");
+        }
+        if (isSparkling){
+            System.out.println("Sparkling");
+        }
+        System.out.println("-------------------\n");
+    }
+
 }
--- src/vending_machine/Snack.java
+++ src/vending_machine/Snack.java
@@ -4,4 +4,17 @@ public class Snack {
     String name;
     int caloriesPerHundredGrams;
     double weight;
+
+    void print(){
+        System.out.println("-----------------");
+        System.out.println(name);
+        System.out.println(weight + " g");
+        System.out.println(calculateCalories() + " calories");
+        System.out.println("-----------------");
+    }
+
+    double calculateCalories(){
+        return weight * caloriesPerHundredGrams / 100;
+    }
+
 }
