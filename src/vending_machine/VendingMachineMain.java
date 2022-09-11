07b90b86aeddd246a48513b95a8cc51c729cf55c
Alvaro Valenzuela <alvarovgerena@gmail.com>
2022-09-11 13:13:25 +0200

    ArrayLists, also scanner for user inputs

--- src/vending_machine/VendingMachineMain.java
+++ src/vending_machine/VendingMachineMain.java
@@ -1,19 +1,40 @@
 package vending_machine;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class VendingMachineMain {
+    static final int MAX_BEVERAGES = 10;
     public static void main(String[] args) {
 
         ArrayList<Beverage> beverages = new ArrayList<>();
+        ArrayList<Snack> snacks = new ArrayList<>();
 
-        System.out.println("Number of elements in beverages array list: " + beverages.size());
-        Beverage beverage = new Beverage();
-        beverages.add(beverage);
-        System.out.println("Number of elements in beverages array list: " + beverages.size());
+        printAmountOfArrayListElements(beverages);
+
+        for (int i = 0; i < MAX_BEVERAGES; i++) {
+            Scanner scanner = new Scanner(System.in);
+            String newName;
+            boolean newIsHot, newIsSparkling;
+            System.out.print("Please enter drink name: ");
+            newName = scanner.nextLine();
+            System.out.print("\nIs it hot?: ");
+            newIsHot = scanner.nextBoolean();
+            System.out.print("\nIs it sparkling?: ");
+            newIsSparkling = scanner.nextBoolean();
+            beverages.add(new Beverage(newName, newIsHot, newIsSparkling));
+        }
+
+        printAmountOfArrayListElements(beverages);
+
+        for (Beverage beverage : beverages) {
+            beverage.print();
+        }
+
+
     }
 
-    static void printAmountOfBeverages(){
-        System.out.println("Number of elements in beverages array list: " + beverages.size());
+    static void printAmountOfArrayListElements(ArrayList list){
+        System.out.println("Number of elements in beverages array list: " + list.size());
     }
 }
